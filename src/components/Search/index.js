bac8402b67fa5e4c25a20a9fb806ba7f525b96b1
angelorubin <angelorubin@gmail.com>
2018-03-22 17:26:50 -0300

    Recriando o componente Search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {searchCity} from './services'
+import City from '../City'
+import styled from 'styled-components'
 
-const Search = props => {
-    return (
-        <div>
-            <input
-                style={{ 'padding' :'5px'}}
-                type="text"
-                size="30"
-                placeholder="Insira aqui o nome da cidade"
-                onBlur={props.search}
-            />
-        </div>
-    )
+const StyledSearch = styled.div`
+    display: grid;
+    grid-gap: 1rem;
+`
+
+const StyledInput = styled.input`
+    padding: 0.5rem;
+    border: none;
+    min-width: 250px;
+    width: minmax(250px, 1fr);
+`
+
+class Search extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            searchData: [],
+            show: false
+        }
+    }
+    searchHandler = event => {
+        searchCity(event.target.value)
+        .then( data => {
+            this.setState({ searchData: data.data.query.results.channel, show: true })
+        })
+        .catch( error => console.log(error))
+    }
+    componentDidUpdate(){
+        // console.log(this.state.searchData.location)
+    }
+    render () {
+        return (
+            <StyledSearch>
+                {
+                    this.state.show ? <City searchData={this.state.searchData}/> : false
+                }
+                <StyledInput
+                    type="text"
+                    placeholder="Insira aqui o nome da cidade"
+                    onBlur={this.searchHandler}
+                />
+            </StyledSearch>
+        )
+    }
 }
 
 export default Search
